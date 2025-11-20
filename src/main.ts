aa55742d2aa45bcd7505b57e3a712ce744baad1c
Isaac David Palomo Peralta <isaac.palomo-p@mail.escuelaing.edu.co>
2025-11-19 23:39:49 -0500

    Configuration Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig } from './swagger'; // ← ya no dará error
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  swaggerConfig(app); // ← activa Swagger
+
   await app.listen(3000);
   console.log('[INFO] Server is running on http://localhost:3000');
 }
